867c1e4d8b069bdef1ee3b10451f7c9271119ea5
compsciAC <68939058+compsciAC@users.noreply.github.com>
2022-09-28 12:08:50 +0100

    Problem7

--- Problem7.py
+++ Problem7.py
@@ -1,1 +1,22 @@
+from math import ceil, log
 
+def primeNumbers(limit):
+    primeList = [2]
+    isNotPrime = set()
+    for i in range(3, limit, 2):
+        if i in isNotPrime:
+            continue
+
+        for j in range(i*3, limit, i*2):
+            isNotPrime.add(j)
+
+        primeList.append(i)
+    return primeList
+
+def primeLimit(number):
+    return ceil(number * (log(number) + log(log(number))))
+
+def nthPrime(number):
+    primes = list(primeNumbers(primeLimit((number+1))))
+    return primes[number - 1]
+    
